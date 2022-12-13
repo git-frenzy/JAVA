67c893a0dd31b29206f12a786b5492b77f04cfe7
Kanishk Suri <kanishksuri29@gmail.com>
2022-12-14 03:03:30 +0530

    update

--- CruxOnline/src/lecture4/RecursionDemo.java
+++ CruxOnline/src/lecture4/RecursionDemo.java
@@ -1,16 +1,24 @@
 package lecture4;
 
+import java.util.Scanner;
+
 public class RecursionDemo {
+    static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        PD(5);
+        System.out.print("input: ");
+        int n1 = scn.nextInt();
+        PD(n1);
     }
+
     public static void PD(int n) {
+
         if (n == 0) {
             return;
         }
         System.out.println(n);
-
         PD(n - 1);
+
+
     }
 }
